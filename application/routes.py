002737a3b8e88507edfbe11dedcc8f0030dab73f
knorris-82 <kimberly@thedigitalsprout.com>
2025-06-19 12:10:15 -0400

    Dependents

--- application/routes.py
+++ application/routes.py
@@ -14,21 +14,8 @@ def index():
 
 @app.route("/whiskeyRecommend", methods=["GET", "POST"])
 def whiskeyRecommend():
-    #let's get the form input
     whiskey = request.form.get("whiskey_name")
-
-    #converting the data to json
-    input_data = json.dumps({"whiskey_name": whiskey})
-
-    #url to send the data to our model
-    url = "http://localhost:5000/api"
-
-    #now we do our post to the url
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=input_data, headers=headers)
-
-    results_json = response.json() #parsing the json response
-    print("Response JSON from backend")
-    print(results_json)
-
+    results_json = {
+        "recommendations": ["Mock Whisky A", "Mock Whisky B", "Mock Whisky C"]
+    }
     return render_template("index.html", results=results_json)
